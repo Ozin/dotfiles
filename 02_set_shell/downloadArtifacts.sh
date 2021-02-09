c8a47e3d8a5c372c096f8f3d41438556f9c8930f
Ozin <Ozin@users.noreply.github.com>
2021-02-09 10:41:13 +0100

    Switch to JetBrainsMono Nerd Font

--- 02_set_shell/downloadArtifacts.sh
+++ 02_set_shell/downloadArtifacts.sh
@@ -6,5 +6,9 @@ mkdir -p "../downloads"
 curl -L https://raw.githubusercontent.com/MartinSeeler/iterm2-material-design/master/material-design-colors.itermcolors > "../downloads/material-design-colors.itermcolors"
 
 # install nerd font
-curl -L https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete%20Mono.ttf > "../downloads/Roboto Mono Nerd Font Complete Mono.ttf"
-open "../downloads/Roboto Mono Nerd Font Complete Mono.ttf"
+
+# curl -L https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete%20Mono.ttf > "../downloads/Roboto Mono Nerd Font Complete Mono.ttf"
+# open "../downloads/Roboto Mono Nerd Font Complete Mono.ttf"
+
+curl -L https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf > "../downloads/JetBrains Mono Regular Nerd Font Complete.ttf"
+open "../downloads/JetBrains Mono Regular Nerd Font Complete.ttf"
