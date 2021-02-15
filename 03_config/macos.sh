afe35880a74e6868bf00e91f4e39c92d9c18d2a9
Ozin <Ozin@users.noreply.github.com>
2021-02-15 10:46:52 +0100

    Add MacOs settings

---
+++ 03_config/macos.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env fish
+
+# Inspired by https://github.com/mathiasbynens/dotfiles/blob/main/.macos
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Set language and text formats
+# Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
+# `Inches`, `en_GB` with `en_US`, and `true` with `false`.
+defaults write NSGlobalDomain AppleLanguages -array "de" "en"
+defaults write NSGlobalDomain AppleLocale -string "de_DE@currency=EUR"
+defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
+defaults write NSGlobalDomain AppleMetricUnits -bool true
+
+# Set the timezone; see `sudo systemsetup -listtimezones` for other values
+sudo systemsetup -settimezone "Europe/Berlin" > /dev/null
+
+# Stop iTunes from responding to the keyboard media keys
+# Also stops media keys from working
+# launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
