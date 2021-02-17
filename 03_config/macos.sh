3a7af27dd64dbfe19fce41b120ec06b6e524bb04
Ozin <Ozin@users.noreply.github.com>
2021-02-17 11:39:52 +0100

    Clear keyboard shortcut for searchManPages

--- 03_config/macos.sh
+++ 03_config/macos.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env fish
 
-# Inspired by https://github.com/mathiasbynens/dotfiles/blob/main/.macos
+# Inspired by:
+#   - https://github.com/mathiasbynens/dotfiles/blob/main/.macos
+#   - https://ryanmo.co/2017/01/05/setting-keyboard-shortcuts-from-terminal-in-macos/
+#   - https://github.com/acourtneybrown/dotfiles/blob/main/script/macos
 
 # Ask for the administrator password upfront
 sudo -v
@@ -27,3 +30,14 @@ sudo systemsetup -settimezone "Europe/Berlin" > /dev/null
 # Stop iTunes from responding to the keyboard media keys
 # Also stops media keys from working
 # launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
+
+# Clear keyboard shortcut for Shift-Cmd-A (Search man page in index in Terminal.app)
+defaults write pbs.plist NSServicesStatus -dict-add "com.apple.Terminal - Search man Page Index in Terminal - searchManPages" '{
+  "enabled_services_menu" = 0;
+  "enabled_context_menu" = 0;
+  "key_equivalent" = "";
+  "presentation_modes" = {
+    "ContextMenu" = 0;
+    "ServicesMenu" = 0;
+  };
+}'
