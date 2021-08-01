3ab489663a9ce20390458b3c4ec96920fbf7985f
Ozin <Ozin@users.noreply.github.com>
2021-08-01 20:13:03 +0200

    Update VSCode extensions

--- 03_config/vscode.sh
+++ 03_config/vscode.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env fish
 
-code --install-extension Equinusocio.vsc-material-theme
-code --install-extension skyapps.fish-vscode
+
+set vscode_plugins \
+    Equinusocio.vsc-material-theme \
+    esbenp.prettier-vscode \
+    jkjustjoshing.vscode-text-pastry \
+    ms-azuretools.vscode-docker \
+    ms-kubernetes-tools.vscode-kubernetes-tools \
+    oderwat.indent-rainbow
+    skyapps.fish-vscode \
+    yzhang.markdown-all-in-one
+
+for plugin in $vscode_plugins
+    code --install-extension $plugin
+end
 
 rm ~/Library/Application\ Support/Code/User/settings.json
 ln -sv (pwd)"/03_config/files/vscode_settings.json" ~/Library/Application\ Support/Code/User/settings.json
