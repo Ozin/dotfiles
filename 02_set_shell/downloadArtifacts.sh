e82df3647138fa246df0391bc0fde41462e6b288
Ozin <Ozin@users.noreply.github.com>
2023-11-14 18:00:15 +0100

    Add alternative font idea

--- 02_set_shell/downloadArtifacts.sh
+++ 02_set_shell/downloadArtifacts.sh
@@ -12,3 +12,6 @@ curl -L https://raw.githubusercontent.com/MartinSeeler/iterm2-material-design/ma
 
 curl -L https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf > "../downloads/JetBrains Mono Regular Nerd Font Complete.ttf"
 open "../downloads/JetBrains Mono Regular Nerd Font Complete.ttf"
+
+# alterative font
+# https://monaspace.githubnext.com/
